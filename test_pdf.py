75caf0d82cadc5ca318bce648c4d73db8af73f8b
famez <f.amez1992@gmail.com>
2025-05-24 13:31:34 +0200

    Removed files

--- test_pdf.py
+++
@@ -1,6 +0,0 @@
-import sys, pathlib, pymupdf
-fname = "uploads/20250512_231853.pdf"  # get document filename
-with pymupdf.open(fname) as doc:  # open document
-    text = chr(12).join([page.get_text() for page in doc])
-# write as a binary file to support non-ASCII characters
-print(text)
