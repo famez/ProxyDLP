151a7fa8ea5354a6daa29670e0f7164114c7c475
famez <f.amez1992@gmail.com>
2025-05-28 19:50:32 +0200

    Return code 401 when accessing to unauthorized page.

--- web/middleware/authMiddleware.js
+++ web/middleware/authMiddleware.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 function authMiddleware(req, res, next) {
   const token = req.cookies.token;
-  if (!token) return res.redirect('/login');
+  if (!token) return res.status(401).redirect('/login');
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
