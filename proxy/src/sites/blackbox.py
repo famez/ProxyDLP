e043141b29cc1be34f39bbe84e1443a17d63a5d0
famez <f.amez1992@gmail.com>
2025-07-27 15:25:30 +0200

    BlackboxAI. When a used is logged in, relate the uploaded files with the current logged user.

--- proxy/src/sites/blackbox.py
+++ proxy/src/sites/blackbox.py
@@ -14,8 +14,10 @@ class BlackBox(Site):
                  allow_anonymous_access, anonymous_conversation_callback):
         super().__init__("BlackBox", urls, account_login_callback, account_check_callback, conversation_callback, attached_file_callback,
                          allow_anonymous_access, anonymous_conversation_callback)
-        
-    
+
+        self.sessions = {}
+        self.workspaces = {}
+            
     def on_request_handle(self, flow):
 
         if flow.request.method == "POST" and "blackbox.ai/api/chat" in flow.request.pretty_url:
@@ -62,18 +64,85 @@ class BlackBox(Site):
                 for message in reversed(json_body['messages']): 
                     if 'role' in message and message['role'] == "user" and 'content' in message:
                         if email:
+                            session_id = json_body['id']
+
+                            results = [item for item in self.sessions if session_id in item]
+
+                            if results:
+                                self.sessions[session_id]['email'] = email
+                                ctx.log.info("Added session 1")
+                            else:
+                                self.sessions[session_id] = {'email': email}        #Keep track of email from conversation id.
+                                ctx.log.info("Added session 2")
+
                             self.conversation_callback(json_body['session']['user']['email'], message['content'])
                         else:
                             self.anonymous_conversation_callback(message['content'])
                         break
 
 
-
             except Exception as e:
                 ctx.log.error(f"[Error] Failed to decompress or parse JSON: {e}")
 
-        
-        elif flow.request.method == "POST" and "blackbox.ai/api/workspace" in flow.request.pretty_url:
+        elif flow.request.method == "POST" and "blackbox.ai/api/workspace/link-to-chat" in flow.request.pretty_url:
+            json_body = flow.request.json()
+
+            ctx.log.info(f"json_body: {json.dumps(json_body, indent=2)}")
+
+            session_id = json_body['chatId']
+            workspace_id = json_body['workspaceIds'][0]
+
+            ctx.log.info(f"session id: {session_id}, workspace_id: {workspace_id}")
+
+            for session in self.sessions:
+                ctx.log.info(f"Session: {str(session)}")
+            
+
+            if session_id in self.sessions:
+                self.sessions[session_id]['workspace'] = json_body['workspaceIds'][0]
+                ctx.log.info(f"Eeeeooo")
+
+                email = None
+
+                if "email" in self.sessions[session_id]:
+                    email = self.sessions[session_id]['email']
+
+                if workspace_id in self.workspaces:
+                    self.sessions[session_id]['files'] = self.workspaces[workspace_id]
+                    ctx.log.info("Session...")
+                    ctx.log.info(str(self.sessions[session_id]))
+
+                    for file in self.sessions[session_id]['files']:
+
+                        self.attached_file_callback(email, file['filename'], file['filepath'], file['content_type'])    #Send file attached event 
+
+
+
+    def on_response_handle(self, flow):
+
+        if flow.request.method == "POST" and "https://www.blackbox.ai/api/workspace" == flow.request.pretty_url:
+
+            workspace_id = None
+
+            response_content_type = flow.response.headers.get("Content-Type", "")
+
+            ctx.log.info("Eooooo one two three")
+
+            if "application/json" in response_content_type.lower():
+
+                try:
+
+                    # Try to parse as JSON
+                    content = flow.response.json()
+
+                    ctx.log.info("Hellooo")
+
+                    if "id" in content:
+                        workspace_id = content["id"]
+                        self.workspaces[workspace_id] = []
+
+                except Exception as e:
+                    ctx.log.error(f"[Error] Failed to decompress or parse JSON: {e}")
             
             content_type = flow.request.headers.get("content-type", "")
 
@@ -93,4 +162,10 @@ class BlackBox(Site):
                         f.write(file['content'])
                     ctx.log.info(f"Saved file: {filepath}")
 
-                    self.attached_file_callback(None, file['filename'], filepath, file['content_type'])
+                    self.workspaces[workspace_id].append({"filename": file['filename'], "filepath": filepath, "content_type": 
+                                                          file['content_type']})
+
+                    ctx.log.info("Adding workspace!!!")
+
+                    #self.attached_file_callback(None, file['filename'], filepath, file['content_type'])
+                    
