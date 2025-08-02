866f7c69aa6ec3b2944484ab5a9f6c3f423d5e82
famez <f.amez1992@gmail.com>
2025-08-02 19:05:38 +0200

    Enhance Gemini integration with non anonymous conversations

--- proxy/src/sites/gemini.py
+++ proxy/src/sites/gemini.py
@@ -6,6 +6,7 @@ import urllib.parse
 import json
 import os
 import uuid
+import re
 
 
 class Gemini(Site):
@@ -15,6 +16,7 @@ class Gemini(Site):
         super().__init__("Google Gemini", urls, account_login_callback, account_check_callback, conversation_callback, attached_file_callback,
                          allow_anonymous_access, anonymous_conversation_callback)
         
+        self.related_user_data = {}
     
     def on_request_handle(self, flow):
             
@@ -36,11 +38,48 @@ class Gemini(Site):
 
                     conversation = parsed[0][0]
 
-                    ctx.log.info(f'Anonymous conversation: {conversation}')
+                    ctx.log.info(f'Conversation: {conversation}')
 
-                    self.anonymous_conversation_callback(conversation)
+                    sid_cookie = flow.request.cookies.get("SID")
+                    ctx.log.info(f"SID cookie value: {sid_cookie}")
+
+                    email = self.related_user_data.get("sid_cookie", {}).get("email", None)
+                    ctx.log.info(f"Email: {email}")
+
+
+                    if email and email != "":
+                        self.conversation_callback(email, conversation)
+
+                    else:
+                        self.anonymous_conversation_callback(conversation)
 
                 except Exception as e:
                     ctx.log.error(f"Could not parse JSON: {e}\nDecoded String:\n{decoded}")
 
-            
+
+    def on_response_handle(self, flow):
+
+        if flow.request.method == "GET" and "gemini.google.com/app" in flow.request.pretty_url:
+
+            sid_cookie = flow.request.cookies.get("SID")
+            ctx.log.info(f"SID cookie value: {sid_cookie}")
+
+            html = flow.response.get_text()
+            #ctx.log.info(f"HTML response body:\n{html}")
+
+            # Look for the anchor tag with the specific href pattern
+            match = re.search(
+                r'aria-label=\"[^\"]*?\(([^)]+)\)\"\shref=\"https:\/\/accounts\.google\.com\/SignOutOptions[^\"]*\"',
+                html,
+                re.DOTALL
+            )
+            if match:
+
+                email = match.group(1)
+                ctx.log.info(f"Extracted email: {email}")
+
+                self.related_user_data['sid_cookie'] = {'email': email}
+                    
+            else:
+
+                ctx.log.info("No email found in anchor content.")
