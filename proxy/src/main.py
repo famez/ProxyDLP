7a3757d7ebecfe4ded4b772c7cc24d303da2ab6b
famez <f.amez1992@gmail.com>
2025-08-09 00:17:37 +0200

    Implement DeepL event handling to prevent rapid successive events

--- proxy/src/main.py
+++ proxy/src/main.py
@@ -41,6 +41,43 @@ def allow_anonymous_access(site):
 
 #Anonymous conversations
 def anonymous_conversation_callback(site, content, source_ip, conversation_id):
+
+    #Workaround for DeepL to avoid receiving several successive events in few seconds
+    if site.get_name() == "DeepL":
+        ctx.log.info("Hello 1")
+
+        latest_event = events_collection.find_one(
+            {"site": "DeepL"},
+            sort=[("timestamp", -1)]
+        )
+
+        latest_timestamp = latest_event.get("timestamp")
+
+        if latest_timestamp:
+            ctx.log.info("Hello 2")
+            if isinstance(latest_timestamp, str):
+                ctx.log.info("Hello 3")
+                latest_timestamp = datetime.fromisoformat(latest_timestamp)
+                
+            # Ensure latest_timestamp is timezone-aware (UTC)
+            if latest_timestamp.tzinfo is None:
+                latest_timestamp = latest_timestamp.replace(tzinfo=timezone.utc)
+            now = datetime.now(timezone.utc)
+
+            #If the event was sent less than 10 seconds ago and the new event content contains the latest one, update the last event content to the current one.
+            if (now - latest_timestamp).total_seconds() < 10 and latest_event.get("content") in content:
+                ctx.log.info("Hello 4")
+                events_collection.update_one({"_id": latest_event["_id"]}, {"$set": {"content": content, "timestamp": now}})
+
+                #Retrigger monitor analysis
+                mon_message = monitor_pb2.EventID(id=str(latest_event["_id"]))
+                ctx.log.info("Sent event to monitor...")
+                response = stub.EventAdded(mon_message)
+                ctx.log.info(f"Response: {response}")
+                return
+            
+                
+
     event = {"timestamp": datetime.now(timezone.utc), "rational": "Conversation", "content": content, "site": site.get_name(), "source_ip": source_ip}
     
     if conversation_id:
