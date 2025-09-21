a49e14e7509190625c2a339e828cc5b2cae1645b
famez <f.amez1992@gmail.com>
2025-09-21 11:41:30 +0200

    Added authenticated conversations to Perplexity site.

--- proxy/src/sites/perplexity.py
+++ proxy/src/sites/perplexity.py
@@ -1,6 +1,7 @@
-from proxy import Site, EmailNotFoundException, decode_jwt, extract_substring_between
-from mitmproxy import http, ctx
-from mitmproxy.http import Response
+from proxy import Site
+from mitmproxy import ctx
+
+import json
 
 
 class Perplexity(Site):
@@ -9,6 +10,8 @@ class Perplexity(Site):
                  allow_anonymous_access, anonymous_conversation_callback):
         super().__init__("Perplexity", urls, account_login_callback, account_check_callback, conversation_callback, attached_file_callback,
                          allow_anonymous_access, anonymous_conversation_callback)
+        
+        self.related_user_data = {}
     
     def on_request_handle(self, flow):
             
@@ -17,5 +20,33 @@ class Perplexity(Site):
             json_body = flow.request.json()
             conversation = json_body.get('query_str', None)
 
+            user_id = json_body['params']['user_nextauth_id']
+
+            email = self.related_user_data.get(user_id, {}).get("email", None)
+
             if isinstance(conversation, str):
-                self.anonymous_conversation_callback(conversation)
+                if email:
+                    self.conversation_callback(email, conversation)
+                else:
+                    self.anonymous_conversation_callback(conversation)
+
+    def on_response_handle(self, flow):
+
+        if flow.request.method == "GET" and "perplexity.ai/api/auth/session" in flow.request.pretty_url:
+
+            content_type = flow.response.headers.get("Content-Type", "")
+            
+            if "application/json" in content_type.lower():
+
+                try:
+
+                    # Try to parse as JSON
+                    content = json.loads(flow.response.content.decode('utf-8'))
+
+                    email = content['user']['email']
+                    user_id = content['user']['id']
+
+                    self.related_user_data[user_id] = {'email': email}
+
+                except Exception as e:
+                    ctx.log.error(f"[Error] Failed to decompress or parse JSON: {e}")
